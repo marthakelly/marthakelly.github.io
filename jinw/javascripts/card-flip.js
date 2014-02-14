74b8e74954c61539063d80ae3145512b2dafa8ba
Martha <martha@uber.com>
2014-02-14 08:57:57 -0800

    hackday!

---
+++ jinw/javascripts/card-flip.js
@@ -0,0 +1,18 @@
+/*
+ * $(document).ready() is a helper function that waits until the jQuery library is fully loaded in your Browser before it tries to use it.
+*/
+$(document).ready(function() {
+  /*
+   * This function waits and listens for a "click" to happen on the div with the class name "hackentines-day-card-container"
+  */
+  $(".giraff").on("click", function() {
+    $(".giraff").toggle();
+    $(".nerd_love").toggle();
+    /*
+      If you like, Try this instead!
+      $(".card").slideToggle();
+      or, try this!
+      $(".card").fadeToggle();
+    */
+  });
+});
